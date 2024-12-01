78020bd364735be8adaf0771cb24c57c3dd06263
zhengjieliewUCAM <138018082+zhengjieliewUCAM@users.noreply.github.com>
2024-12-01 22:00:47 +0000

    Update static_config.py

--- config/static_config.py
+++ config/static_config.py
@@ -1,8 +1,12 @@
-# Dataset directories (relative paths)
-dataset_base_dir = './dataset'
-train_directory = f'{dataset_base_dir}/train'
-val_directory = f'{dataset_base_dir}/val'
-test_directory = f'{dataset_base_dir}/test'
+import os
+
+# User-defined or default dataset directory
+dataset_base_dir = os.getenv('DATASET_DIR', './dataset')
+
+# Subdirectories for train, validation, and test splits
+train_directory = os.path.join(dataset_base_dir, 'train')
+val_directory = os.path.join(dataset_base_dir, 'val')
+test_directory = os.path.join(dataset_base_dir, 'test')
 
 # Parameters
 seed = 17
