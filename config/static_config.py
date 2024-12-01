d21c76be6c8b2b57f8c00b1f4be902c4fe01d15c
zhengjieliewUCAM <138018082+zhengjieliewUCAM@users.noreply.github.com>
2024-12-01 21:51:41 +0000

    Create static_config.py

---
+++ config/static_config.py
@@ -0,0 +1,15 @@
+# Dataset directories (relative paths)
+dataset_base_dir = './dataset'
+train_directory = f'{dataset_base_dir}/train'
+val_directory = f'{dataset_base_dir}/val'
+test_directory = f'{dataset_base_dir}/test'
+
+# Parameters
+seed = 17
+batch_size = 32
+target_size = (224, 224)
+class_names = ['dispersion', 'undissolved', 'dissolved', 'gel', 'swelling']
+class_indices = {name: i for i, name in enumerate(class_names)}
+
+# Checkpoint path
+checkpoint_path = './checkpoints/base2dcnnmodel.keras'
