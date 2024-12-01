d0ff9fff25dedc2af485fd0538d2dc2d5870cf98
zhengjieliewUCAM <138018082+zhengjieliewUCAM@users.noreply.github.com>
2024-12-01 23:51:25 +0000

    Create main.py

---
+++ dynamic_inference/3dcnnbase/main.py
@@ -0,0 +1,17 @@
+from dynamic_inference.3dcnnbase.train import main as train_main
+from dynamic_inference.3dcnnbase.data_processing import save_video_frames_parallel
+from config.dynamic_3dcnnbase_config import base_data_dir, classes
+
+def preprocess_videos():
+    save_video_frames_parallel(
+        data_dir=base_data_dir,
+        save_dir=base_data_dir,
+        classes=classes,
+        frame_size=(224, 224),
+        num_frames=15,
+        max_workers=4
+    )
+
+if __name__ == "__main__":
+    preprocess_videos()
+    train_main()
