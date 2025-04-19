ff5e523a2d6d5be9a0855e85d2a443fc7e9bfd1f
zjlucam <138018082+zjlucam@users.noreply.github.com>
2025-04-19 01:05:13 +0100

    Update dynamic_3dcnnbase_config.py

--- config/dynamic_3dcnnbase_config.py
+++ config/dynamic_3dcnnbase_config.py
@@ -9,14 +9,15 @@ val_dir = os.path.join(base_data_dir, 'val')
 classes = ['Dissolved', 'Undissolved', 'Gel', 'Swelling', 'Dispersion']
 
 # Frame and Video Parameters
-FRAME_SIZE = (224, 224)  # Frame dimensions
-FRAMES_PER_VIDEO = 15    # Number of frames per video clip
+FRAME_SIZE = (224, 224) 
+FRAMES_PER_VIDEO = 15  
 
 # Training Parameters
 BATCH_SIZE = 16
 EPOCHS = 150
 LEARNING_RATE = 0.001
+SEED = 17  
 
 # Checkpoints
-model_checkpoint_path = './checkpoints/base_3dcnn.keras'
+checkpoint_path = './checkpoints/base_3dcnn.keras' 
 weights_checkpoint_path = './checkpoints/base_3dcnn.weights.h5'
